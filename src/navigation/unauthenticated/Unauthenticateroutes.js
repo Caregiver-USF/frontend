6558cf0147ceffa7ae90a9cba30925f6394e42a6
Jaffer  Syed <jaffersyed@Jaffers-MacBook-Pro.local>
2020-07-09 19:16:25 -0400

    some stuff

--- src/navigation/unauthenticated/Unauthenticateroutes.js
+++ src/navigation/unauthenticated/Unauthenticateroutes.js
@@ -1,18 +1,50 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import Home from '../../pages/unauthenticated/Home/Home';
 import Login from '../../pages/unauthenticated/Login/Login';
 import Signup from '../../pages/unauthenticated/Signup/Signup';
+import { connect } from 'react-redux';
 
 
-const UnauthenticatedRoutes = () => {
+
+const UnauthenticatedRoutes = (props) => {
+    const [authenticatedState , setAuthenticatedState] = useState(false);
+    useEffect(() => {
+        const { authenticated } = props;
+        if (authenticated === true) {
+            setAuthenticatedState(true);
+        }
+    }, [props])
+
+    const CompileUnAuthenticatedRoutes = () => {
+        if (authenticatedState === false) {
+            return (
+                <div>
+                 <Route path="/" component={Home} exact />
+                 <Route path="/login" component={Login} exact />
+                 <Route path="/signup" component={Signup} exact />
+                </div>
+            )
+        }  else {
+            return <Redirect to="/dashboard" />;
+        }
+    }
+
     return (
         <div>
-         <Route path="/" component={Home} exact />
-         <Route path="/login" component={Login} exact />
-         <Route path="/signup" component={Signup} exact />
+         <CompileUnAuthenticatedRoutes />
         </div>
     )
 }
 
-export default UnauthenticatedRoutes;
+const mapStateToProps =({
+    user: {
+        authenticated
+    }
+}) => {
+    return {
+        authenticated
+    }
+}
+
+export default connect(mapStateToProps , null)(UnauthenticatedRoutes);
